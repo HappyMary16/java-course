214cd09db291d2c1264233f940975c905cce5a1e
mborodin <m.borodin.1999@gmail.com>
2026-04-09 21:32:47 +0200

    Add materials for lab 6 (exceptions)

---
+++ lab6-exceptions/src/test/java/com/mborodin/javacourse/CoffeeShopTest.java
@@ -0,0 +1,40 @@
+package com.mborodin.javacourse;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoffeeShopTest {
+
+    @Test
+    void buyCoffee_coffeePresent() {
+    }
+
+    @Test
+    void buyCoffee_coffeeNotPresent() {
+    }
+
+    @Test
+    void buyCoffeeIfPresent_coffeePresent() {
+    }
+
+    @Test
+    void buyCoffeeIfPresent_coffeeNotPresent() {
+    }
+
+    @Test
+    void giveFeedback_goodFeedback() {
+    }
+
+    @Test
+    void giveFeedback_badFeedback() {
+    }
+
+    @Test
+    void readFeedbacks() {
+    }
+
+    @Test
+    void coffeeAmount() {
+    }
+}
